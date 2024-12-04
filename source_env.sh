3c2c822ce7053709e0fb592c1c636d056fb52960
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2024-12-04 17:14:31 +0100

    add GPU dependencies setup (cudnn)

--- source_env.sh
+++ source_env.sh
@@ -1,4 +1,5 @@
 
 # for python CLI entry point to be available from arbitrary location
 export PATH=/path/to/cryo-BCR/bin:${PATH}
-
+# for CUDA/GPU-based devices recongnition
+export LD_LIBRARY_PATH=/path/to/micromamba/envs/cryobcr-env/lib/python3.9/site-packages/nvidia/cudnn/lib/:${LD_LIBRARY_PATH}
